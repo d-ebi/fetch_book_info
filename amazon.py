411744be15d5bb9c1d54b741895ccf787a6bff4c
d-ebi <daiki.ebisawa.p@gmail.com>
2018-02-13 21:55:18 +0900

    xpathのみ外だししていたが,テキストを取得するところまでを関数として外だしするよう変更

--- amazon.py
+++ amazon.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 import urllib.request, urllib.parse, urllib.error
 import sys
 import time
@@ -19,7 +20,7 @@ def fetch_parsed_web_page(url, retry=3, charset='utf-8'):
             break
         except urllib.error.HTTPError as e:
             if http_error_count >= retry:
-                raise urllib.error.HTTPError(e.code, e.reason, e.headers)
+                raise urllib.error.HTTPError(e.code, e.reason, e.headers, e.hdrs, e.fp)
             else:
                 time.sleep(sleep_time)
                 http_error_count += 1
@@ -29,27 +30,27 @@ def fetch_parsed_web_page(url, retry=3, charset='utf-8'):
 def main():
     # TODO: あとで標準入力から受けるように変える
     # print(sys.argv[1])
-    ISBN     = '9784776209676'
+    ISBN     = sys.argv[1] #'9784776209676'
 
     dom = fetch_parsed_web_page(amazon.base_url.format(ISBN = ISBN))
-    search_results_first_item = dom.xpath(amazon.xpaths['search_results_extract'].format(number = '0'))[0]
-    item_detail_url = search_results_first_item.get('href')
-    print(item_detail_url)
+    item_detail_url = amazon.extract_search_results(dom)
     
     # TODO:検索結果にて,Kindle版がある場合,Kindle版ではない方を拾うようにしないといけない
     # TODO: dom2は無いので変える,上と下で明らかに処理が違うので関数を分離
     dom2 = fetch_parsed_web_page(item_detail_url)
-    print('書籍名      :' + dom2.xpath(amazon.xpaths['book_name'])[0].text)
-    print('本の種別    :' + dom2.xpath(amazon.xpaths['classification'])[0].text)
-    print('発行年月日  :' + dom2.xpath(amazon.xpaths['issued_date'])[0].text)
-    for author in dom2.xpath(amazon.xpaths['authors']):
-        print('著者名      :' + author.text)
-    print('金額        :' + dom2.xpath(amazon.xpaths['price'])[0].text)
-    print('おすすめ度  :' + dom2.xpath(amazon.xpaths['recommended_degree'])[0].text)
-    print('出版社      :' + dom2.xpath(amazon.xpaths['publisher'])[0].tail)
-    for category in dom2.xpath(amazon.xpaths['categories']):
-        print('カテゴリ階層:' + category.text_content())
-    print('ページ数    :' + dom2.xpath(amazon.xpaths['number_of_pages'])[0].text_content())
+    print('URL         :' + item_detail_url)
+    print('書籍名      :' + amazon.extract_product_name(dom2))
+    print('本の種別    :' + amazon.extract_classification(dom2))
+    print('発行年月日  :' + amazon.extract_issued_date(dom2))
+    print(dom2.xpath('//span[contains(@class, "author")]'))
+    for author in amazon.extract_authors(dom2):
+        print('著者名      :' + author)
+    print('金額        :' + amazon.extract_price(dom2))
+    print('おすすめ度  :' + amazon.extract_recommended_degree(dom2))
+    print('出版社      :' + amazon.extract_publisher(dom2))
+    for category in amazon.extract_categories(dom2):
+        print('カテゴリ階層:' + str(category))
+    print('ページ数    :' + amazon.extract_number_of_pages(dom2))
     
 if __name__ == '__main__':
     main()
--- web_pages/amazon.py
+++ web_pages/amazon.py
@@ -1,4 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import re
+
 base_url = 'https://www.amazon.co.jp/s/ref=nb_sb_noss?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Daps&field-keywords={ISBN}'
+
+def extract_search_results(dom, number=0):
+    xpath = '//li[@id="result_{number}"]//a[contains(@class, "s-access-detail-page")]'.format(number = str(number))
+    extract = dom.xpath(xpath)[0]
+    return extract.get('href') 
+
+def extract_product_image(dom):
+    pass
+
+def extract_product_name(dom):
+    xpath = '//span[@id="productTitle"]'
+    extract = dom.xpath(xpath)[0]
+    return extract.text
+
+def extract_classification(dom):
+    xpath ='//h1[@id="title"]/span[2]' 
+    extract = dom.xpath(xpath)[0]
+    return extract.text
+
+def extract_issued_date(dom):
+    xpath = '//h1[@id="title"]/span[3]'
+    extract = dom.xpath(xpath)[0]
+    return re.search(r'(\d{4}/\d{1,2}/\d{1,2})', extract.text).group(1)
+
+def extract_authors(dom):
+    xpath = '//span[contains(@class, "author")]//a[contains(@href, "dp_byline_sr_book") or contains(@href, "dp_byline_cont_book")]'
+    extract = dom.xpath(xpath)
+    authors = list()
+    for author in extract:
+        authors.apppend(author.text)
+    return authors
+        
+def extract_price(dom):
+    xpath = '//div[@id="buyNewSection"]//span[contains(@class, "a-color-price")]'
+    extract = dom.xpath(xpath)[0]
+    return extract.text[2:].replace(',', '')
+
+def extract_recommended_degree(dom):
+    xpath = '//div[@id="averageCustomerReviews"]//span[@class="a-icon-alt"]'
+    extract = dom.xpath(xpath)[0]
+    return extract.text.replace('5つ星のうち ', '')
+
+def extract_publisher(dom):
+    xpath = '//div[@id="detail_bullets_id"]//*[contains(text(), "出版社")]'
+    extract = dom.xpath(xpath)[0]
+    return re.search(r' (.+?) \(.+?\)', extract.tail).group(1)
+
+def extract_categories(dom):
+    xpath = '//li[@id="SalesRank"]//span[@class="zg_hrsr_ladder"]'
+    extract = dom.xpath(xpath)
+    categories = list()
+    for category in extract:
+        exclude_invalid_str = category.text_content()[2:]
+        categories.append(exclude_invalid_str.split(' > '))
+    return categories
+
+def extract_number_of_pages(dom):
+    xpath = '//div[@id="detail_bullets_id"]//*[contains(text(), "ページ")]'
+    extract = dom.xpath(xpath)[0]
+    return re.search(r'(\d+)ページ', extract.text_content()).group(1)
+
 xpaths = {
     # numberは検索結果の何個目かを示す. 0オリジン.
     'search_results_extract': '//li[@id="result_{number}"]//a[contains(@class, "s-access-detail-page")]',
